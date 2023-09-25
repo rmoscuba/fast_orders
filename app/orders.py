a82fbf4e1f6e32590d9fca42d33825f1e919c780
Rodolfo Ortega <rmoscuba@gmail.com>
2023-09-25 13:08:47 -0400

    Add all case

--- app/orders.py
+++ app/orders.py
@@ -1,5 +1,7 @@
 
 def process_orders(orders, criterion):
-            
-    filtered_orders = filter(lambda item: item.status == criterion, orders)
+    if criterion != 'all':
+        filtered_orders = filter(lambda item: item.status == criterion, orders)
+    else:
+        filtered_orders = orders
     return sum(o.price*o.quantity for o in filtered_orders)
--- test/test_main.py
+++ test/test_main.py
@@ -11,6 +11,17 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
 
+json_orders_data_str_2399_55 = """{
+   "orders": [
+       {"id": 1, "item": "Laptop", "quantity": 1, "price": 999.99, "status": "completed"},
+       {"id": 2, "item": "Smartphone", "quantity": 2, "price": 499.95, "status": "pending"},
+       {"id": 3, "item": "Headphones", "quantity": 3, "price": 99.90, "status": "completed"},
+       {"id": 4, "item": "Mouse", "quantity": 4, "price": 24.99, "status": "canceled"}
+   ],
+   "criterion": "all"
+}
+"""
+
 json_orders_data_str_1299_69 = """{
    "orders": [
        {"id": 1, "item": "Laptop", "quantity": 1, "price": 999.99, "status": "completed"},
@@ -77,6 +88,12 @@ json_orders_data_str_bad_status = """{
    "criterion": "completed"
 }
 """
+
+def test_orders_all():
+    json_orders_data = json.loads(json_orders_data_str_2399_55)
+    response = client.post("/solution", json=json_orders_data)
+    assert response.status_code == 200
+    assert response.text =='"2399.55"'
 
 def test_orders_completed():
     json_orders_data = json.loads(json_orders_data_str_1299_69)
