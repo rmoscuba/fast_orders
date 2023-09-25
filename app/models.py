dc0b25a3dc84bacd6280344a616929cd2987c46f
Rodolfo Ortega <rmoscuba@gmail.com>
2023-09-25 15:48:13 -0400

    Add validators

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from decimal import Decimal
 from typing import List
 
@@ -9,6 +9,24 @@ class Item(BaseModel):
     price: Decimal | None = None
     status: str | None = None
 
+    @validator("price")
+    def validate_price(cls, v):
+        if v < 0:
+            raise ValueError("Price must be anpositive Decimal value")
+        return v
+    
+    @validator("status")
+    def validate_status(cls, v):
+        if v not in ['completed','pending','canceled']:
+            raise ValueError("Status must be 'completed','pending' or 'canceled'")
+        return v
+
 class OrderList(BaseModel):
     orders: List[Item]
     criterion: str | None = None
+
+    @validator("criterion")
+    def validate_status(cls, v):
+        if v not in ['completed','pending','canceled','all']:
+            raise ValueError("Status must be 'completed','pending', 'canceled' or 'all'")
+        return v
